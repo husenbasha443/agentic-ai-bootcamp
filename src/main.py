398131620ac0903e2010d7fd7c075fef583a5665
husenbasha443 <23ca032.husen@sjec.ac.in>
2025-12-19 18:30:06 +0530

    Basic Project commit

---
+++ src/main.py
@@ -0,0 +1,23 @@
+"""
+This module contains test cases.
+"""
+
+def print_hi(name):
+    """
+    Print a greeting message.
+
+    Args:
+        name (str): Name of the person to greet.
+    """
+    print(f"Hi, {name}")
+
+
+def main():
+    """
+    Main function.
+    """
+    print_hi("PyCharm")
+
+
+if __name__ == "__main__":
+    main()
